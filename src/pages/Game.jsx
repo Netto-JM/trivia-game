4c94d27d7746b7531cc56e6a2003ebf0b53f95d5
NettoJM <nettojm86@gmail.com>
2023-01-15 19:56:11 -0300

    fazendo requisição do jogo ao montar o componente, faz logout se o token for inválido e remove o token do localStorage

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { fetchGame, INVALID_TOKEN_ERROR } from '../redux/actions';
 
 class Game extends Component {
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(fetchGame());
+  }
+
   render() {
+    const { questions, errorMessage, history } = this.props;
+    if (errorMessage === INVALID_TOKEN_ERROR) {
+      localStorage.removeItem('token');
+      history.push('/');
+    }
+
+    console.log('questions: ', questions);
+
     return (
       <div>
         <h1>Game</h1>
@@ -12,4 +28,16 @@ class Game extends Component {
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+  questions: state.game.questions,
+  errorMessage: state.game.errorMessage,
+});
+
+Game.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+  questions: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  history: PropTypes.shape().isRequired,
+};
+
+export default connect(mapStateToProps)(Game);
