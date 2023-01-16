7eb2b4159e92a3fc5dcea18b9fcb6ba2fdcb4a9c
NettoJM <nettojm86@gmail.com>
2023-01-15 21:31:37 -0300

    removido score do reducer user

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -3,7 +3,6 @@ import { SAVE_USER } from '../actions';
 const INITIAL_STATE = {
   image: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
   name: '',
-  score: 0,
 };
 
 const userReducer = (state = INITIAL_STATE, { type, payload }) => {
