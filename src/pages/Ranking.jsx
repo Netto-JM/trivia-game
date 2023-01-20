95a2d2f7dd4c917a4c008c6823800a16984148b8
NettoJM <nettojm86@gmail.com>
2023-01-19 22:59:42 -0300

    conclui a tela de Ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,53 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { changeQuestionIndex, clearPlayerInfo } from '../redux/actions';
 
 class Ranking extends Component {
+  state = {
+    triviaRanking: [],
+  };
+
+  componentDidMount() {
+    const triviaRanking = JSON.parse(localStorage.getItem('triviaRanking')) || [];
+    this.setState({ triviaRanking: triviaRanking.sort((a, b) => b.score - a.score) });
+    console.log('sortedRank', triviaRanking);
+  }
+
+  handleClick = () => {
+    const { dispatch, history } = this.props;
+    dispatch(changeQuestionIndex(0));
+    dispatch(clearPlayerInfo());
+    history.push('/');
+  };
+
   render() {
-    const { player } = this.props;
-    const image = <img src={ `https://www.gravatar.com/avatar/${player.image}` } alt={ player.name } />;
+    const { triviaRanking } = this.state;
+    // const image = <img src={ imgUrl } alt={ player.name } />;
+    const tableRanking = triviaRanking.map(({ image, name, score }, index) => {
+      const imgUrl = `https://www.gravatar.com/avatar/${image}`;
+      return (
+        <tr key={ image + name + (score * Math.random()) }>
+          <td>
+            <img src={ imgUrl } alt={ name } />
+          </td>
+          <td data-testid={ `player-name-${index}` }>{name}</td>
+          <td data-testid={ `player-score-${index}` }>{score}</td>
+        </tr>
+      );
+    });
+
     return (
       <>
         <table data-testid="ranking-title">
           <thead>
             <tr>
-              <th data-testid="picture">Profile</th>
-              <th data-testid="player-name">Player Name</th>
-              <th data-testid="score-ranking">Pontuação</th>
+              <th>Profile</th>
+              <th>Player Name</th>
+              <th>Pontuação</th>
               {/* <th>Excluir</th> */}
             </tr>
           </thead>
           <tbody>
-            <tr key={ player }>
-              <td>
-                {image}
-              </td>
-              <td data-testid="player-name-0">
-                { player.name }
-              </td>
-              <td data-testid="player-score-0">
-                { player.score }
-              </td>
-              {/* <td>
-                <button
-                  type="button"
-                  data-testid="delete-btn"
-                  // onClick={ () => dispatch(delExpense(expense)) }
-                  value="Excluir"
-                >
-                  Excluir
-                </button>
-              </td> */}
-            </tr>
+            {tableRanking}
           </tbody>
         </table>
         <button
           type="button"
           data-testid="btn-go-home"
-          onClick={ () => {
-            const { history } = this.props;
-            history.push('/');
-          } }
+          onClick={ this.handleClick }
         >
           Logout
         </button>
@@ -62,6 +70,7 @@ const mapStateToProps = ({ player }) => ({
 
 Ranking.propTypes = {
   history: PropTypes.shape(),
+  dispatch: PropTypes.func,
 }.isRequired;
 
 export default connect(mapStateToProps)(Ranking);
